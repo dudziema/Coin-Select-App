7b3722e366821bc2773f84ac32aad518570dc5ef
Małgorzata Dudziec <dudziec.malgorzata@gmail.com>
2022-02-06 21:50:09 +0100

    configuration and removing the init project data

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,20 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import HomePage from "../views/HomePage.vue";
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
+    name: "HomePage",
+    component: HomePage,
   },
   {
     path: "/about",
-    name: "About",
+    name: "AboutMe",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import(/* webpackChunkName: "about" */ "../views/AboutMe.vue"),
   },
 ];
 
