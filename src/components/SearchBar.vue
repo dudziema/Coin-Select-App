2810183ba7f6fafb07ffb26a93ff0e5ddc2182ca
Małgorzata Dudziec <dudziec.malgorzata@gmail.com>
2022-02-06 23:03:18 +0100

    Searchbar component created

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container--searchbar--input">
+    <input type="text" v-model="search" placeholder="Search coin" />
+    <label>Search coin:</label>
+  </div>
+</template>
+<script>
+export default {
+  name: "SearchBar",
+};
+</script>
