0d933ae9931feb5e1d486ccefb04d20add9ff802
Małgorzata Dudziec <dudziec.malgorzata@gmail.com>
2022-02-08 11:11:43 +0100

    creation new component CoinDetails

---
+++ src/views/CoinDetails.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Logo Coin title (symbol)</h1>
+    <p>Website adress:</p>
+    <p>Year of release:</p>
+    <p>Description:</p>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      coin: null,
+    };
+  },
+  created() {
+    //fetch coin and set local data
+  },
+};
+</script>
